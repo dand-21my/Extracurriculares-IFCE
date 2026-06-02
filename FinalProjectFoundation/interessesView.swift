1cec13eb977ac6ee05b6eb241d0237e1a580906d
dandeires Atacadista <dandara.vic.flower@gmail.com>
2026-06-02 16:52:27 -0300

    tabbar, tela inicial

---
+++ FinalProjectFoundation/interessesView.swift
@@ -0,0 +1,18 @@
+//
+//  interessesView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 02/06/26.
+//
+
+import SwiftUI
+
+struct interessesView: View {
+    var body: some View {
+       
+    }
+}
+
+#Preview {
+    interessesView()
+}
---
+++ FinalProjectFoundation/locaisView.swift
@@ -0,0 +1,18 @@
+//
+//  locaisView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 02/06/26.
+//
+
+import SwiftUI
+
+struct locaisView: View {
+    var body: some View {
+    
+    }
+}
+
+#Preview {
+    locaisView()
+}
