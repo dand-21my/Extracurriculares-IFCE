03f2f54e89cb6b0a37d0368bdd4d42d6b9aaf7bc
theusss10 <matheus.oliveira10@aluno.ifce.edu.br>
2026-06-16 17:17:04 -0300

    Corrigi erros no codigo

---
+++ FinalProjectFoundation/ArteCultura.swift
@@ -0,0 +1,46 @@
+//
+//  ArteCultura.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 16/06/26.
+//
+
+import SwiftUI
+import SwiftData
+import SwiftDataSQLite
+
+@SQLiteTable("ArteCultura")
+@Model
+class ArteCultura: Identifiable {
+    var id: String
+    var nome: String
+    var orientador: String
+    var local: String
+    var dias: String
+    var horario: String
+    var ensino: String
+    var contato: String
+    var foto: Data
+
+    init(
+        id: String,
+        nome: String,
+        orientador: String,
+        local: String,
+        dias: String,
+        horario: String,
+        ensino: String,
+        contato: String,
+        foto: Data
+    ) {
+        self.id = id
+        self.nome = nome
+        self.orientador = orientador
+        self.local = local
+        self.dias = dias
+        self.horario = horario
+        self.ensino = ensino
+        self.contato = contato
+        self.foto = foto
+    }
+}
